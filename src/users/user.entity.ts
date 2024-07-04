e73cf88647137baa67d6c82c42c1dec140c9b683
Gabriel Feijó <92588394+GabrielFeijo@users.noreply.github.com>
2024-07-04 13:38:43 -0300

    chore: update user entity

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,13 +1,26 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class User {
+  @ApiProperty({
+    example: 1,
+    description: 'The ID of the User.',
+  })
   @PrimaryGeneratedColumn()
   id: number;
 
+  @ApiProperty({
+    example: 'Gabriel Feijó',
+    description: 'The name of the User.',
+  })
   @Column()
   name: string;
 
+  @ApiProperty({
+    example: 'email@email.com',
+    description: 'The email address of the User.',
+  })
   @Column()
   email: string;
 }
